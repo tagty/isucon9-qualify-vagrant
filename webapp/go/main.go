df985a9fdf1e89f548ade2911e0ac63278e57915
tagty <bouzu.ao@gmail.com>
2021-08-09 15:19:06 +0900

    add: new relic

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -18,6 +18,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
+	"github.com/newrelic/go-agent/v3/newrelic"
 	goji "goji.io"
 	"goji.io/pat"
 	"golang.org/x/crypto/bcrypt"
@@ -63,6 +64,8 @@ var (
 	templates *template.Template
 	dbx       *sqlx.DB
 	store     sessions.Store
+	app       *newrelic.Application
+	err       error
 )
 
 type Config struct {
@@ -278,7 +281,23 @@ func init() {
 	))
 }
 
+func patGet(path string, f func(w http.ResponseWriter, r *http.Request)) (*pat.Pattern, func(w http.ResponseWriter, r *http.Request)) {
+	path, handleFunc := newrelic.WrapHandleFunc(app, path, f)
+	return pat.Get(path), handleFunc
+}
+
+func patPost(path string, f func(w http.ResponseWriter, r *http.Request)) (*pat.Pattern, func(w http.ResponseWriter, r *http.Request)) {
+	path, handleFunc := newrelic.WrapHandleFunc(app, path, f)
+	return pat.Post(path), handleFunc
+}
+
 func main() {
+	app, err = newrelic.NewApplication(
+		newrelic.ConfigAppName("isucon9-qualify-vagrant"),
+		newrelic.ConfigLicense("a072d91ece9cbe8d7204ed41b3f061f235ab6818"),
+		newrelic.ConfigDistributedTracerEnabled(true),
+	)
+
 	host := os.Getenv("MYSQL_HOST")
 	if host == "" {
 		host = "127.0.0.1"
@@ -287,7 +306,7 @@ func main() {
 	if port == "" {
 		port = "3306"
 	}
-	_, err := strconv.Atoi(port)
+	_, err = strconv.Atoi(port)
 	if err != nil {
 		log.Fatalf("failed to read DB port number from an environment variable MYSQL_PORT.\nError: %s", err.Error())
 	}
@@ -322,38 +341,38 @@ func main() {
 	mux := goji.NewMux()
 
 	// API
-	mux.HandleFunc(pat.Post("/initialize"), postInitialize)
-	mux.HandleFunc(pat.Get("/new_items.json"), getNewItems)
-	mux.HandleFunc(pat.Get("/new_items/:root_category_id.json"), getNewCategoryItems)
-	mux.HandleFunc(pat.Get("/users/transactions.json"), getTransactions)
-	mux.HandleFunc(pat.Get("/users/:user_id.json"), getUserItems)
-	mux.HandleFunc(pat.Get("/items/:item_id.json"), getItem)
-	mux.HandleFunc(pat.Post("/items/edit"), postItemEdit)
-	mux.HandleFunc(pat.Post("/buy"), postBuy)
-	mux.HandleFunc(pat.Post("/sell"), postSell)
-	mux.HandleFunc(pat.Post("/ship"), postShip)
-	mux.HandleFunc(pat.Post("/ship_done"), postShipDone)
-	mux.HandleFunc(pat.Post("/complete"), postComplete)
-	mux.HandleFunc(pat.Get("/transactions/:transaction_evidence_id.png"), getQRCode)
-	mux.HandleFunc(pat.Post("/bump"), postBump)
-	mux.HandleFunc(pat.Get("/settings"), getSettings)
-	mux.HandleFunc(pat.Post("/login"), postLogin)
-	mux.HandleFunc(pat.Post("/register"), postRegister)
-	mux.HandleFunc(pat.Get("/reports.json"), getReports)
+	mux.HandleFunc(patPost("/initialize", postInitialize))
+	mux.HandleFunc(patGet("/new_items.json", getNewItems))
+	mux.HandleFunc(patGet("/new_items/:root_category_id.json", getNewCategoryItems))
+	mux.HandleFunc(patGet("/users/transactions.json", getTransactions))
+	mux.HandleFunc(patGet("/users/:user_id.json", getUserItems))
+	mux.HandleFunc(patGet("/items/:item_id.json", getItem))
+	mux.HandleFunc(patPost("/items/edit", postItemEdit))
+	mux.HandleFunc(patPost("/buy", postBuy))
+	mux.HandleFunc(patPost("/sell", postSell))
+	mux.HandleFunc(patPost("/ship", postShip))
+	mux.HandleFunc(patPost("/ship_done", postShipDone))
+	mux.HandleFunc(patPost("/complete", postComplete))
+	mux.HandleFunc(patGet("/transactions/:transaction_evidence_id.png", getQRCode))
+	mux.HandleFunc(patPost("/bump", postBump))
+	mux.HandleFunc(patGet("/settings", getSettings))
+	mux.HandleFunc(patPost("/login", postLogin))
+	mux.HandleFunc(patPost("/register", postRegister))
+	mux.HandleFunc(patGet("/reports.json", getReports))
 	// Frontend
-	mux.HandleFunc(pat.Get("/"), getIndex)
-	mux.HandleFunc(pat.Get("/login"), getIndex)
-	mux.HandleFunc(pat.Get("/register"), getIndex)
-	mux.HandleFunc(pat.Get("/timeline"), getIndex)
-	mux.HandleFunc(pat.Get("/categories/:category_id/items"), getIndex)
-	mux.HandleFunc(pat.Get("/sell"), getIndex)
-	mux.HandleFunc(pat.Get("/items/:item_id"), getIndex)
-	mux.HandleFunc(pat.Get("/items/:item_id/edit"), getIndex)
-	mux.HandleFunc(pat.Get("/items/:item_id/buy"), getIndex)
-	mux.HandleFunc(pat.Get("/buy/complete"), getIndex)
-	mux.HandleFunc(pat.Get("/transactions/:transaction_id"), getIndex)
-	mux.HandleFunc(pat.Get("/users/:user_id"), getIndex)
-	mux.HandleFunc(pat.Get("/users/setting"), getIndex)
+	mux.HandleFunc(patGet("/", getIndex))
+	mux.HandleFunc(patGet("/login", getIndex))
+	mux.HandleFunc(patGet("/register", getIndex))
+	mux.HandleFunc(patGet("/timeline", getIndex))
+	mux.HandleFunc(patGet("/categories/:category_id/items", getIndex))
+	mux.HandleFunc(patGet("/sell", getIndex))
+	mux.HandleFunc(patGet("/items/:item_id", getIndex))
+	mux.HandleFunc(patGet("/items/:item_id/edit", getIndex))
+	mux.HandleFunc(patGet("/items/:item_id/buy", getIndex))
+	mux.HandleFunc(patGet("/buy/complete", getIndex))
+	mux.HandleFunc(patGet("/transactions/:transaction_id", getIndex))
+	mux.HandleFunc(patGet("/users/:user_id", getIndex))
+	mux.HandleFunc(patGet("/users/setting", getIndex))
 	// Assets
 	mux.Handle(pat.Get("/*"), http.FileServer(http.Dir("../public")))
 	log.Fatal(http.ListenAndServe(":8000", mux))
